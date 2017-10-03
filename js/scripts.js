9e6c72fe9f4847b3c90bbf35fa8b2404e505c218
Kimberly Bordon <kbordon@gmail.com>
2017-10-02 20:04:06 -0700

    Experiment with pen scrolling horizontally.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-  $('.parallax').parallax();
+  // $('.parallax').parallax();
+  var offset=$("#stickytest").offset();
+  console.log(offset); 
+  $(window).scroll(function(event){
+    var st = $(this).scrollTop();
+    $("#stickytest").css("left", st + offset.left);
+    $("#stickytest").css("top", st + offset.top);
+  })
 });
